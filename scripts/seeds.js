386f5e56e09e2a0e7aaef8be6a40afb4acddc878
Igor Berlenko <igor@berlenko.com>
2019-10-07 14:09:08 +0800

    add cli script to compile contracts #15

---
+++ scripts/seeds.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const compile = require('./compile')
+
+program
+  .command('compile <contract>')
+  .description('Compile custom contract')
+  .action(async function (contract) {
+    await compile({
+      contract: contract,
+      source: `./src/${contract}.cpp`
+    })
+    
+    console.log(`${contract} compiled`)
+  })
+  
+program.parse(process.argv)
