fbaf0ee18b3d1ca6f571eee96c52b726ea4c8bab
n13 <nheger@gmail.com>
2019-10-08 07:47:49 +0800

    cleanup

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -4,10 +4,7 @@ const program = require('commander')
 const compile = require('./compile')
 const deploy = require('./deploy.command')
 
-program
-  .command('compile <contract>')
-  .description('Compile custom contract')
-  .action(async function (contract) {
+const compileAction = async (contract) => {
     try {
       await compile({
         contract: contract,
@@ -17,12 +14,9 @@ program
     } catch (err) {
         console.log("compile failef for ", contract, " error: ", err)
     }
-  })
+}
 
-program
-  .command('deploy <contract>')
-  .description('Deploy custom contract')
-  .action(async function (contract) {
+const deployAction = async (contract) => {
     try {
       await deploy(contract)
       console.log(`${contract} deployed`)
@@ -30,26 +24,32 @@ program
       console.log("error deploying ", contract)
       console.log(err)
     }
+}
+
+const runAction = async (contract) => {
+  await compileAction(contract)
+  await deployAction(contract)
+}
+
+program
+  .command('compile <contract>')
+  .description('Compile custom contract')
+  .action(async function (contract) {
+    await compileAction(contract)
+  })
+
+program
+  .command('deploy <contract>')
+  .description('Deploy custom contract')
+  .action(async function (contract) {
+    await deployAction(contract)
   })
 
   program
   .command('run <contract>')
   .description('compile and deploy custom contract')
   .action(async function (contract) {
-    try {
-      await compile({
-        contract: contract,
-        source: `./src/seeds.${contract}.cpp`
-      })
-      console.log(`${contract} compiled`)
-
-      await deploy(contract)
-      console.log(`${contract} deployed`)
-    
-    } catch(err) {
-      console.log("error running ", contract)
-      console.log(err)
-    }
+    await runAction(contract)
   })
   
 program.parse(process.argv)
