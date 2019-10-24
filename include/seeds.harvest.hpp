4b326b96cc5f9cb58a8b5de75c99f12ce42b4a8d
n13 <nheger@gmail.com>
2019-10-24 22:02:22 +0800

    publish claimrefund #65

--- include/seeds.harvest.hpp
+++ include/seeds.harvest.hpp
@@ -163,7 +163,7 @@ extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action) {
       execute_action<harvest>(name(receiver), name(code), &harvest::plant);
   } else if (code == receiver) {
       switch (action) {
-          EOSIO_DISPATCH_HELPER(harvest, (reset)(runharvest)(unplant)(claimreward)(cancelrefund)(sow)(calcrep)(calctrx)(calcplanted))
+          EOSIO_DISPATCH_HELPER(harvest, (reset)(runharvest)(unplant)(claimreward)(claimrefund)(cancelrefund)(sow)(calcrep)(calctrx)(calcplanted))
       }
   }
 }
