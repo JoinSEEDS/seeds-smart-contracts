d0dc8f318f08db9aa3abe21fe1c8cb8a78d4254f
n13 <nheger@gmail.com>
2019-12-13 00:13:55 +0700

    backup users and show accounts scripts

--- scripts/backupusers.js
+++ scripts/backupusers.js
@@ -1,22 +1,58 @@
 const fs = require('fs')
 const { eos, names } = require('./helper')
 
-const { accounts } = names
+const { accounts, token } = names
+
+const getBalanceFloat = async (user) => {
+  const balance = await eos.getCurrencyBalance(names.token, user, 'SEEDS')
+  let num = parseFloat(balance[0])
+  if (num == NaN) {
+    console.log("nil balance for "+user+": "+JSON.stringify(balance))
+  }
+  return parseFloat(balance[0])
+}
 
 const main = async () => {
   const { owner } = accounts
+
+  console.log("BACKING UP users for "+accounts + " and " + token)
 
   const users = await eos.getTableRows({
     code: accounts,
     scope: accounts,
     table: 'users',
     json: true,
-    limit: 100
+    limit: 300
   })
 
-  fs.writeFileSync('users_backup.json', JSON.stringify(users))
+  fs.writeFileSync('users_backup_'+accounts+'.json', JSON.stringify(users, null, 2))
 
-  console.log('done')
+  console.log('users: '+JSON.stringify(users, null,2))
+
+  let user_accounts = users.rows.map(({ account }) => account )
+
+  console.log('accounts: '+JSON.stringify(user_accounts, null,2))
+
+  console.log("saved "+user_accounts.length + " users")
+
+  let account_balances = []
+  
+  for (let i=0; i<user_accounts.length; i++) {
+    let acct = user_accounts[i]
+    let balance = await getBalanceFloat(acct)
+    console.log(acct+": "+balance)
+    account_balances.push({
+      account: acct,
+      balance: balance
+    })
+  }
+
+  console.log('balances: '+JSON.stringify(account_balances, null,2))
+
+  fs.writeFileSync('user_balances_'+token+'.json', JSON.stringify(account_balances, null, 2))
+
+
+
 }
 
 main()
---
+++ scripts/showaccounts.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const { eos, names } = require('./helper')
+
+const fillString = (str, toLength) => {
+  var s = str
+  while (s.length < toLength) {
+      s = s + " "
+  }
+  return s
+}
+
+const main = async () => {
+
+  console.log("LIST ACCOUNTS")
+
+  for (const [key, value] of Object.entries(names)) {
+    //console.log(key +": "+ JSON.stringify(value, null, 2));
+
+    const info = await eos.getAccount(value)
+    //console.log(value + ": "+JSON.stringify(info, null, 2))
+
+    let ramused = info.ram_usage 
+    let ram = info.total_resources.ram_bytes
+    let cpu = info.total_resources.cpu_weight
+    let net = info.total_resources.net_weight
+
+    let cpu_used = info.cpu_limit.used
+    let cpu_available = info.cpu_limit.available
+
+    let cpu_percent_str = parseInt((cpu_used / cpu_available)*100) + "%"
+    let cpu_str = "CPU "+cpu_used + " / " +cpu_available+ "  " + parseInt((cpu_used / cpu_available)*100) + "%"
+
+    let net_used = info.net_limit.used
+    let net_available = info.net_limit.available
+    
+    let net_percent_str = parseInt((net_used / net_available)*100) + "%"
+    let net_str = "NET: "+net_used + " / " +net_available+ "  " + parseInt((net_used / net_available)*100) + "%"
+
+    console.log(
+      fillString(value, 12) + " " + 
+      fillString("RAM: "+parseInt(ramused/1000) + " / "+parseInt(ram/1000) + " KB" , 24) + " " +
+      fillString("CPU: "+cpu + " "+cpu_percent_str, 30) + 
+      "NET: "+net +" "+net_percent_str)
+
+    console.log(fillString(" ", 38) + fillString(cpu_str, 30) + fillString(net_str, 30) )
+      
+
+  }
+  // for (let i=0; i<names.length; i++) {
+  //   let name = names[i]
+  //   console.log("info on "+name)
+
+
+  //   console.log(name + ": "+JSON.stringify(info, null, 2))
+
+  // }
+
+  //fs.writeFileSync('user_balances_'+token+'.json', JSON.stringify(account_balances, null, 2))
+
+
+
+}
+
+main()
