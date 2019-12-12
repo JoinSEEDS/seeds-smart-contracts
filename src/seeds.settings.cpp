7d39763b1f09f59c90d577abc32dc6abf25262e4
n13 <nheger@gmail.com>
2019-12-12 12:44:43 +0700

    added contract names to settings; added settings test

--- src/seeds.settings.cpp
+++ src/seeds.settings.cpp
@@ -3,15 +3,28 @@
 void settings::reset() {
   require_auth(_self);
 
+  // config
   configure(name("propminstake"), 25 * 10000);
   configure(name("refsnewprice"), 25 * 10000);
   configure(name("refsmajority"), 80);
   configure(name("refsquorum"), 80);
   configure(name("hrvstreward"), 100000);
+
+  // contracts
+  setcontract(name("accounts"), "accts.seeds"_n);
+  setcontract(name("harvest"), "harvst.seeds"_n);
+  setcontract(name("settings"), "settgs.seeds"_n);
+  setcontract(name("proposals"), "funds.seeds"_n);
+  setcontract(name("invites"), "invite.seeds"_n); // old invite contract
+  setcontract(name("onboarding"), "join.seeds"_n); // new invite contract
+  setcontract(name("referendums"), "rules.seeds"_n);
+  setcontract(name("history"), "histry.seeds"_n);
+  setcontract(name("policy"), "policy.seeds"_n);
+  setcontract(name("token"), "token.seeds"_n);
 }
 
 void settings::configure(name param, uint64_t value) {
-  require_auth(_self);
+  require_auth(get_self());
 
   auto citr = config.find(param.value);
 
@@ -24,6 +37,24 @@ void settings::configure(name param, uint64_t value) {
     config.modify(citr, _self, [&](auto& item) {
       item.param = param;
       item.value = value;
+    });
+  }
+}
+
+void settings::setcontract(name contract, name account) {
+  require_auth(get_self());
+
+  auto citr = contracts.find(contract.value);
+
+  if (citr == contracts.end()) {
+    contracts.emplace(_self, [&](auto& item) {
+      item.contract = contract;
+      item.account = account;
+    });
+  } else {
+    contracts.modify(citr, _self, [&](auto& item) {
+      item.contract = contract;
+      item.account = account;
     });
   }
 }
