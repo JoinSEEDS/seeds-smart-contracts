2eb3697c9d930b4c89bc13f23b67267fe84326e6
n13 <nheger@gmail.com>
2019-12-20 22:53:24 +0800

    removing testnet names - too error prone

--- include/contracts.hpp
+++ include/contracts.hpp
@@ -4,26 +4,6 @@
 
 using eosio::name;
 
-// Always comment out before checking in - we don't want to get this accidentally on mainnet 
-//#define TESTNET = true; // this should be coming from env as compiler flag - only when deploying to testnet
-
-#ifdef TESTNET
-namespace contracts {
-  name accounts =    "acctsxxseeds"_n;
-  name harvest =     "harvstxseeds"_n;
-  name settings =    "settgsxseeds"_n;
-  name proposals =   "fundsxxseeds"_n;
-  name invites =     "invitexseeds"_n;
-  name referendums = "rulesxxseeds"_n;
-  name history =     "histryxseeds"_n;
-  name token =       "tokenxxseeds"_n;
-  name tlostoken = "eosio.token"_n;
-  name policy =      "policyxseeds"_n;
-  name bank =        "systemxseeds"_n;
-  name onboarding =  "joinxxxseeds"_n;
-  name acctcreator = "freexxxseeds"_n;
-}
-#else
 namespace contracts {
   name accounts = "accts.seeds"_n;
   name harvest = "harvst.seeds"_n;
@@ -39,4 +19,3 @@ namespace contracts {
   name onboarding = "join.seeds"_n;
   name acctcreator = "free.seeds"_n;
 }
-#endif
