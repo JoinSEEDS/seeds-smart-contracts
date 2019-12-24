d08192cabd15d362cbf2ce65ddfadacf66cb42ae
n13 <nheger@gmail.com>
2019-12-24 20:57:30 +0800

    added bulk invite

--- scripts/onboarding_test.js
+++ scripts/onboarding_test.js
@@ -1,4 +1,5 @@
 const program = require('commander')
+const fs = require('fs')
 
 const { eos, names, getTableRows, initContracts, sha256, isLocal, ramdom64ByteHexString, createKeypair } = require('../scripts/helper')
 
@@ -19,24 +20,70 @@ const fromHexString = hexString => new Uint8Array(hexString.match(/.{1,2}/g).map
 
 
 
+const bulk_invite = async (sponsor, num, totalAmount) => {
+    var secrets = "Secret,Hash,Seeds (total)\n"
+    const fileName = 'secrets_'+num+'.csv'
+    var log = []
 
+    try {
+        totalAmount = parseInt(totalAmount)
+        for (i = 0; i < num; i++) {
+            console.log("inviting "+(i+1) + "/" + num)
+            let inv = await invite(sponsor, totalAmount, false)
+            console.log("secret "+inv.secret)
+            console.log("hashed secret: "+inv.hashedSecret)
+            log.push(inv)
+            secrets = secrets + inv.secret +"," + inv.hashedSecret + "," + totalAmount + "\n"
+        }
+    
+        console.log("secrets: "+secrets)
+    
+        console.log("log: " + JSON.stringify(log, null, 2))
+    
+    
+        fs.writeFileSync(fileName, secrets)
+        fs.writeFileSync('secrets_log_'+num+'.json', JSON.stringify(log, null,2))
+        
+        console.log(num + " secrets written to "+fileName)
+    
+    } catch (err) {
+        console.log("Bulk Invite Error: "+err)
+
+        fs.writeFileSync("error_"+fileName, secrets)
+        fs.writeFileSync('error_secrets_log_'+num+'.json', JSON.stringify(log, null,2))
+
+    }
+
+
+}
   
-const invite = async (sponsor, sponsorActiveKey) => {
+const invite = async (sponsor, totalAmount, debug = false) => {
     
     const contracts = await initContracts({ onboarding, token, accounts, harvest })
 
-    const transferQuantity = `5.0000 SEEDS`
-    const sowQuantity = '5.0000 SEEDS'
-    const totalQuantity = '10.0000 SEEDS'
+    totalAmount = parseInt(totalAmount)
+
+    const plantedSeeds = 5
+    const transferredSeeds = totalAmount - plantedSeeds
+
+    const transferQuantity = transferredSeeds + `.0000 SEEDS`
+    const sowQuantity = plantedSeeds + '.0000 SEEDS'
+    const totalQuantity = totalAmount + '.0000 SEEDS'
     
+    console.log("transferQuantity " + transferQuantity)
+    console.log("sowQuantity " + sowQuantity)
+    console.log("totalQuantity " + totalQuantity)
+
     const inviteSecret = await ramdom64ByteHexString()
     const inviteHash = sha256(fromHexString(inviteSecret)).toString('hex')
 
-
+    const result = {
+        secret: inviteSecret,
+        hashedSecret: inviteHash
+    }
 
     console.log("Invite Secret: "+inviteSecret)
     console.log("Secret hash: "+inviteHash)
-
     
     const deposit = async () => {
         try {
@@ -58,23 +105,29 @@ const invite = async (sponsor, sponsorActiveKey) => {
 
     await deposit()
 
-    const sponsorsBefore = await getTableRows({
-        code: onboarding,
-        scope: onboarding,
-        table: 'sponsors',
-        json: true
-    })
-    console.log("sponsors after deposit "+JSON.stringify(sponsorsBefore.rows, null, 2))
+    if (debug == true) {
+        const sponsorsBefore = await getTableRows({
+            code: onboarding,
+            scope: onboarding,
+            table: 'sponsors',
+            json: true
+        })
+        console.log("sponsors after deposit "+JSON.stringify(sponsorsBefore.rows, null, 2))    
+    }
 
     await invite()
 
-    const sponsorsAfter = await getTableRows({
-        code: onboarding,
-        scope: onboarding,
-        table: 'sponsors',
-        json: true
-    })
-    console.log("sponsors after invite "+JSON.stringify(sponsorsAfter.rows, null, 2))
+    if (debug == true) {
+        const sponsorsAfter = await getTableRows({
+            code: onboarding,
+            scope: onboarding,
+            table: 'sponsors',
+            json: true
+        })
+        console.log("sponsors after invite "+JSON.stringify(sponsorsAfter.rows, null, 2))
+    }   
+
+    return result
 
 }
 
@@ -111,12 +164,21 @@ const accept = async (newAccount, inviteSecret) => {
 }
 
 program
-  .command('invite <param>')
+  .command('invite <sponsor>')
   .description('invite new user')
-  .action(async function (param) {
-      console.log("invite with " + param)
-    await invite(param)
+  .action(async function (sponsor) {
+      console.log("invite with " + sponsor)
+    await invite(sponsor, 20, true) // always 20 seeds
   })
+
+program
+  .command('bulk_invite <sponsor> <num> <totalAmount>')
+  .description('invite new user')
+  .action(async function (param, num, totalAmount) {
+      console.log("generate "+num+" invites at " + totalAmount + " SEEDS" + " with sponsor " + param)
+    await bulk_invite(param, num, totalAmount)
+  })
+
 
 program
   .command('accept <newAccount> <inviteSecret>')
