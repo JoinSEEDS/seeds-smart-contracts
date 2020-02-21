a41e52ced7aa53768712affebffc42388c1ac7d7
nik <nheger@gmail.com>
2020-02-21 14:52:59 +0700

    half moon cycle for proposals
    cycle times are parameterized in settings so they can be tested / changed

--- include/seeds.proposals.hpp
+++ include/seeds.proposals.hpp
@@ -1,11 +1,13 @@
 #include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
 #include <eosio/transaction.hpp>
+#include <eosio/singleton.hpp>
 #include <seeds.token.hpp>
 #include <contracts.hpp>
 #include <utils.hpp>
 
 using namespace eosio;
+using namespace utils;
 using std::string;
 
 CONTRACT proposals : public contract {
@@ -16,6 +18,7 @@ CONTRACT proposals : public contract {
           props(receiver, receiver.value),
           voice(receiver, receiver.value),
           lastprops(receiver, receiver.value),
+          cycle(receiver, receiver.value),
           config(contracts::settings, contracts::settings.value),
           users(contracts::accounts, contracts::accounts.value)
           {}
@@ -35,8 +38,16 @@ CONTRACT proposals : public contract {
       ACTION against(name user, uint64_t id, uint64_t amount);
 
       ACTION onperiod();
+
+      ACTION decayvoice();
+
   private:
       symbol seeds_symbol = symbol("SEEDS", 4);
+      
+      void update_cycle();
+      void update_voicedecay();
+      uint64_t get_cycle_period_sec();
+      uint64_t get_voice_decay_period_sec();
 
       void check_user(name account);
       void check_citizen(name account);
@@ -109,18 +120,28 @@ CONTRACT proposals : public contract {
         uint64_t primary_key()const { return account.value; }
       };
 
-      typedef eosio::multi_index<"props"_n, proposal_table> proposal_tables;
-      typedef eosio::multi_index<"votes"_n, vote_table> votes_tables;
-      typedef eosio::multi_index<"config"_n, config_table> config_tables;
-      typedef eosio::multi_index<"users"_n, user_table> user_tables;
-      typedef eosio::multi_index<"voice"_n, voice_table> voice_tables;
-      typedef eosio::multi_index<"lastprops"_n, last_proposal_table> last_proposal_tables;
+    TABLE cycle_table {
+      uint64_t propcycle; 
+      uint64_t t_onperiod; // last time onperiod ran
+      uint64_t t_voicedecay; // last time voice was decayed
+    };
+    
+    typedef eosio::multi_index<"props"_n, proposal_table> proposal_tables;
+    typedef eosio::multi_index<"votes"_n, vote_table> votes_tables;
+    typedef eosio::multi_index<"config"_n, config_table> config_tables;
+    typedef eosio::multi_index<"users"_n, user_table> user_tables;
+    typedef eosio::multi_index<"voice"_n, voice_table> voice_tables;
+    typedef eosio::multi_index<"lastprops"_n, last_proposal_table> last_proposal_tables;
+    typedef singleton<"cycle"_n, cycle_table> cycle_tables;
+    typedef eosio::multi_index<"cycle"_n, cycle_table> dump_for_cycle;
 
-      config_tables config;
-      proposal_tables props;
-      user_tables users;
-      voice_tables voice;
-      last_proposal_tables lastprops;
+    config_tables config;
+    proposal_tables props;
+    user_tables users;
+    voice_tables voice;
+    last_proposal_tables lastprops;
+    cycle_tables cycle;
+
 };
 
 extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action) {
@@ -128,7 +149,7 @@ extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action) {
       execute_action<proposals>(name(receiver), name(code), &proposals::stake);
   } else if (code == receiver) {
       switch (action) {
-        EOSIO_DISPATCH_HELPER(proposals, (reset)(create)(update)(addvoice)(favour)(against)(onperiod))
+        EOSIO_DISPATCH_HELPER(proposals, (reset)(create)(update)(addvoice)(favour)(against)(onperiod)(decayvoice))
       }
   }
 }
