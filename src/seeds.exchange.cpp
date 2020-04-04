abec2f31aa6d94fc81f4d48fe2ecfe2c94131fe8
NIK <nheger@gmail.com>
2020-04-04 22:24:59 +0200

    Token Sale Rounds 1 - 50 (#35)
    - Rounds added
    - Init rounds action
    - addround action
    - initsale action
    - Updating the price before and after sale
    - Updating price in config
    - Removed ability to manually set USD price
    - Unit test tests for all cases
    - Unit tests fro purchases crossing over rounds, e.g. buying 1.5 rounds, or buying 0.1 of one round and 0.3 of the next round
    - Edge cases (running out of rounds)

--- src/seeds.exchange.cpp
+++ src/seeds.exchange.cpp
@@ -1,26 +1,86 @@
 #include <seeds.exchange.hpp>
+#include <cmath>
 
 void exchange::reset() {
   require_auth(_self);
 
   config.remove();
 
-  auto pitr = payhistory.begin();
-  while(pitr != payhistory.end()) {
-    pitr = payhistory.erase(pitr);
-  }
-
   asset citizen_limit =  asset(uint64_t(2500000000), seeds_symbol);
   asset resident_limit =  asset(uint64_t(2500000000), seeds_symbol);
   asset visitor_limit =  asset(25000 * 10000, seeds_symbol);
 
-  asset seeds_per_usd =  asset( uint64_t(1000000), seeds_symbol);
   asset tlos_per_usd =  asset(0.03 * 10000, seeds_symbol);
 
   updatelimit(citizen_limit, resident_limit, visitor_limit);
-  updateusd(seeds_per_usd);
   updatetlos(tlos_per_usd);
 
+/**
+ 
+  // NEVER CHECK THESE IN
+  // we never want to erase rounds or sold table or history except for unit testing
+  
+  sold.remove();
+
+  auto pitr = payhistory.begin();
+  while(pitr != payhistory.end()) {
+    pitr = payhistory.erase(pitr);
+  }
+  
+  auto ritr = rounds.begin();
+  while(ritr != rounds.end()){
+    ritr = rounds.erase(ritr);
+  }
+
+/**/
+
+}
+
+asset exchange::seeds_for_usd(asset usd_quantity) {
+  updateprice();
+
+  soldtable s = sold.get_or_create(get_self(), soldtable());
+
+  double usd_total = double(usd_quantity.amount);
+  double usd_remaining = usd_total;
+  double seeds_amount = 0.0;
+
+  auto ritr = rounds.begin(); //rounds.find(p -> current_round);
+
+  uint64_t round_start_volume = 0;
+
+  while(ritr != rounds.end() && usd_remaining > 0) {
+    uint64_t round_end_volume = ritr->max_sold;
+
+    if (s.total_sold < round_end_volume) {
+      double usd_per_seeds = 10000.0 / double(ritr->seeds_per_usd.amount);
+
+      // num available
+      double available_in_round = round_end_volume - std::max(round_start_volume, s.total_sold);
+
+      // price of available seeds
+      double usd_available = available_in_round * usd_per_seeds;
+
+      // if < usd amount remaining -> calculate, add, and exit
+      if (usd_available >= usd_remaining) {
+        seeds_amount += (usd_remaining * ritr->seeds_per_usd.amount) / 10000;
+        usd_remaining = 0;
+        break;
+      } else {
+        usd_remaining -= usd_available;
+        seeds_amount += available_in_round;
+      }
+    }
+    
+    round_start_volume = round_end_volume;
+    ritr++;
+
+    check(ritr != rounds.end(), "not enough funds available. requested USD value: "+std::to_string(usd_total/10000.0) + 
+    " available USD value: "+std::to_string( (usd_total - usd_remaining) / 10000.0) + " max vol: " + std::to_string(round_end_volume));
+
+  }
+
+  return asset(seeds_amount, seeds_symbol);
 }
 
 void exchange::purchase_usd(name buyer, asset usd_quantity, string memo) {
@@ -51,7 +111,8 @@ void exchange::purchase_usd(name buyer, asset usd_quantity, string memo) {
       break;
   }
   
-  uint64_t seeds_amount = (usd_quantity.amount * seeds_per_usd.amount) / 10000;
+  // uint64_t seeds_amount = (usd_quantity.amount * seeds_per_usd.amount) / 10000;
+  uint64_t seeds_amount = seeds_for_usd(usd_quantity).amount;
   asset seeds_quantity = asset(seeds_amount, seeds_symbol);
   
   auto sitr = dailystats.find(buyer.value);
@@ -59,7 +120,7 @@ void exchange::purchase_usd(name buyer, asset usd_quantity, string memo) {
     seeds_purchased = sitr->seeds_purchased;
   }
 
-  check(seeds_limit.amount >= seeds_purchased + seeds_amount, "purchase limit overdrawn, tried to buy " + seeds_quantity.to_string() + " limit: " + seeds_limit.to_string() + " new total would be: " + std::to_string(seeds_purchased + seeds_amount));
+  check(seeds_limit.amount >= seeds_purchased + seeds_amount, "purchase limit overdrawn, tried to buy " + seeds_quantity.to_string() + " limit: " + seeds_limit.to_string() + " new total would be: " + std::to_string( (seeds_purchased + seeds_amount) / 10000.0));
 
   if (sitr == dailystats.end()) {
     dailystats.emplace(get_self(), [&](auto& s) {
@@ -76,10 +137,12 @@ void exchange::purchase_usd(name buyer, asset usd_quantity, string memo) {
   stb.total_sold = stb.total_sold + seeds_amount;
   sold.set(stb, get_self());
 
+  updateprice();
+
   action(
     permission_level{get_self(), "active"_n},
     contracts::token, "transfer"_n,
-    make_tuple(get_self(), buyer, seeds_quantity, string(""))
+    make_tuple(get_self(), buyer, seeds_quantity, memo)
   ).send();    
 }
 
@@ -112,6 +175,8 @@ void exchange::newpayment(name recipientAccount, string paymentSymbol, string pa
     uint64_t key = std::hash<std::string>{}(paymentId);
 
     check( history_by_payment_id.find(key) == history_by_payment_id.end(), "duplicate transaction: "+paymentId);
+
+    string memo = (paymentSymbol + ": " + paymentId).substr(0, 255);
 
     purchase_usd(recipientAccount, usd_asset, paymentId);
 
@@ -150,16 +215,16 @@ void exchange::updatelimit(asset citizen_limit, asset resident_limit, asset visi
   config.set(c, get_self());
 }
 
-void exchange::updateusd(asset seeds_per_usd) {
-  require_auth(get_self());
+// void exchange::updateusd(asset seeds_per_usd) {
+//   require_auth(get_self());
 
-  configtable c = config.get_or_create(get_self(), configtable());
+//   configtable c = config.get_or_create(get_self(), configtable());
   
-  c.seeds_per_usd = seeds_per_usd;
-  c.timestamp = current_time_point().sec_since_epoch();
+//   c.seeds_per_usd = seeds_per_usd;
+//   c.timestamp = current_time_point().sec_since_epoch();
   
-  config.set(c, get_self());
-}
+//   config.set(c, get_self());
+// }
 
 void exchange::updatetlos(asset tlos_per_usd) {
   require_auth(get_self());
@@ -170,4 +235,94 @@ void exchange::updatetlos(asset tlos_per_usd) {
   c.timestamp = current_time_point().sec_since_epoch();
   
   config.set(c, get_self());
+}
+
+void exchange::updateprice() {
+
+  soldtable stb = sold.get_or_create(get_self(), soldtable());
+  uint64_t total_sold = stb.total_sold;
+
+  price_table p = price.get_or_create(get_self(), price_table());
+
+  configtable c = config.get_or_create(get_self(), configtable());
+
+  auto ritr = rounds.begin();
+
+  while(true) {
+    
+    check(ritr != rounds.end(), "No more rounds - sold out");
+
+    if (total_sold < ritr -> max_sold) {
+      p.current_round_id = ritr -> id;
+      p.current_seeds_per_usd = ritr -> seeds_per_usd;
+      p.remaining = ritr->max_sold - total_sold;
+
+      price.set(p, get_self());
+
+      c.seeds_per_usd = ritr -> seeds_per_usd;
+      c.timestamp = current_time_point().sec_since_epoch();
+
+      config.set(c, get_self());
+
+      break;
+    }
+
+    ritr++;
+  }
+}
+
+ACTION exchange::addround(uint64_t volume, asset seeds_per_usd) {
+  require_auth(get_self());
+
+  check(seeds_per_usd.amount > 0, "seeds per usd must be > 0");
+  check(volume > 0, "volume must be > 0");
+
+  uint64_t prev_vol = 0;
+
+  auto rounds_number = std::distance(rounds.begin(), rounds.end());
+
+  auto previtr = rounds.find(rounds_number - 1);
+  if (previtr != rounds.end()) {
+    prev_vol = previtr -> max_sold;
+  } else {
+    check(rounds_number == 0, "invalid round id - must be continuous");
+  }
+
+  rounds.emplace(_self, [&](auto& item) {
+    item.id = rounds_number;
+    item.seeds_per_usd = seeds_per_usd;
+    item.max_sold = prev_vol + volume; 
+  });
+}
+
+ACTION exchange::initsale() {
+  require_auth(get_self());
+  initrounds(
+      uint64_t(1100000) * uint64_t(10000), // "1,100,000.0000 SEEDS"
+      asset(909091, seeds_symbol) // 0.011 USD / SEEDS  ==> 90.9090909091 SEEDS / USD  
+  );
+}
+
+ACTION exchange::initrounds(uint64_t volume_per_round, asset initial_seeds_per_usd) {
+
+  require_auth(get_self());
+
+  auto ritr = rounds.begin();
+  while(ritr != rounds.end()){
+    ritr = rounds.erase(ritr);
+  }
+
+  uint64_t seeds_per_usd = initial_seeds_per_usd.amount;
+  
+  double usd_per_seeds = 1.0 / (seeds_per_usd / 10000.0);
+  double increment_factor = 1.033; // 3.3% per round = factor of 1.033 per round
+
+  for(int i=0; i<50; i++) {
+    addround(volume_per_round, asset(seeds_per_usd, seeds_symbol));
+    usd_per_seeds *= increment_factor;
+    seeds_per_usd = uint64_t(round( 10000.0 / usd_per_seeds));
+  }
+
+  updateprice();
+
 }
--- test/exchange.test.js
+++ test/exchange.test.js
@@ -28,13 +28,17 @@ describe('Exchange', async assert => {
   console.log(`update daily limits`)
   await contracts.exchange.updatelimit("2500.0000 SEEDS", "100.0000 SEEDS", "3.0000 SEEDS", { authorization: `${exchange}@active` })
   
-  console.log(`update USD exchange rate - seeds per USD`)
-  let seeds_per_usd = 101
-  await contracts.exchange.updateusd(""+seeds_per_usd + ".0000 SEEDS", { authorization: `${exchange}@active` })
+  //console.log(`update USD exchange rate - seeds per USD`)
+  let seeds_per_usd = 100
+  //await contracts.exchange.updateusd(""+seeds_per_usd + ".0000 SEEDS", { authorization: `${exchange}@active` })
 
   console.log(`update TLOS rate - tlos per usd`)
   await contracts.exchange.updatetlos("3.0000 SEEDS", { authorization: `${exchange}@active` })
-  
+
+  await contracts.exchange.onperiod({ authorization: `${exchange}@active` })
+
+  await contracts.exchange.initrounds( (1100000) * 10000, "100.0000 SEEDS", { authorization: `${exchange}@active` })  
+
   //console.log(`transfer ${tlosQuantity} from ${firstuser} to ${exchange}`)
   //await contracts.tlostoken.transfer(firstuser, exchange, tlosQuantity, '', { authorization: `${firstuser}@active` })
 
@@ -48,11 +52,25 @@ describe('Exchange', async assert => {
 
   console.log("balance before "+balanceBefore)
 
+  let soldBefore = await eos.getTableRows({
+    code: exchange,
+    scope: exchange,
+    table: 'sold',
+    json: true,
+  })
+
   await contracts.exchange.newpayment(firstuser, "BTC", "0x3affgf", parseInt(usd * 10000), { authorization: `${exchange}@active` })
 
   let balanceAfter = await getBalanceFloat(firstuser)
 
-  console.log("balance before "+balanceAfter)
+  let soldAfter = await eos.getTableRows({
+    code: exchange,
+    scope: exchange,
+    table: 'sold',
+    json: true,
+  })
+
+  console.log("balance after "+balanceAfter)
 
   usd = 99
 
@@ -70,13 +88,11 @@ describe('Exchange', async assert => {
   } catch (err) {
   }
 
-
-
   console.log(`reset daily stats`)
   await contracts.exchange.onperiod({ authorization: `${exchange}@active` })  
-  
-  const seedsBalance = await getBalanceFloat(firstuser)
-  
+
+  expectedSeeds = parseFloat(expectedSeeds.toFixed(4))
+
   assert({
     given: `sent ${usd} USD to exchange`,
     should: `receive ${expectedSeeds} to account going from `+balanceBefore + ' to ' + balanceAfter,
@@ -85,12 +101,18 @@ describe('Exchange', async assert => {
   })
 
   assert({
+    given: 'sent seeds',
+    should: 'update sold',
+    actual: soldAfter.rows[0].total_sold,
+    expected: soldBefore.rows[0].total_sold + expectedSeeds * 10000
+  })
+
+  assert({
     given: `user buys more than they are allowed`,
     should: `fail`,
     actual: canExceedBalance,
     expected: false
   })
-
 
   assert({
     given: `newpurchase called multiple times with same transaction`,
@@ -106,4 +128,473 @@ describe('Exchange', async assert => {
   //   actual: seedsBalance,
   //   expected: Number.parseInt(seedsQuantity)
   // })
+})
+
+describe('Token Sale Rounds', async assert => {
+
+  const contracts = await initContracts({ accounts, token, exchange })
+  
+  console.log(`reset exchange`)
+  await contracts.exchange.reset({ authorization: `${exchange}@active` })  
+
+  console.log(`reset accounts`)
+  await contracts.accounts.reset({ authorization: `${accounts}@active` })
+
+  console.log(`add user`)
+  await contracts.accounts.adduser(firstuser, 'First user', "individual", { authorization: `${accounts}@active` })
+
+  console.log(`transfer seeds to ${exchange}`)
+  //await contracts.token.transfer(firstuser, exchange, "2000000.0000 SEEDS", 'unit test', { authorization: `${firstuser}@active` })
+
+  console.log(`update daily limits`)
+  await contracts.exchange.updatelimit("2500000.0000 SEEDS", "10000.0000 SEEDS", "3.0000 SEEDS", { authorization: `${exchange}@active` })
+  
+  console.log(`update TLOS rate - tlos per usd`)
+  await contracts.exchange.updatetlos("3.0000 SEEDS", { authorization: `${exchange}@active` })
+      
+  console.log(`init token sale rounds`)
+
+  const getRounds = async () => {
+    let rounds = await eos.getTableRows({
+      code: exchange,
+      scope: exchange,
+      table: 'rounds',
+      json: true,
+      limit: 100,
+    })
+
+    //console.log("rounds "+JSON.stringify(rounds, null, 2))
+
+    return rounds
+  }
+
+  await getRounds()
+
+  console.log("test init rounds - 100 seeds per round")
+  await contracts.exchange.initrounds( (100) * 10000, "90.9091 SEEDS", { authorization: `${exchange}@active` })  
+
+  let rounds = await getRounds()
+
+  rounds = rounds.rows.map( ( item ) => { return { 
+    round: item.id,
+    seeds_per_usd: parseFloat(item.seeds_per_usd), 
+    usd_rate: (1.0 / parseFloat(item.seeds_per_usd)).toFixed(5) } 
+  })
+
+  let usd_rates = rounds.map( ({usd_rate})  => usd_rate )
+
+  //console.log("rounds "+JSON.stringify(rounds, null, 2))
+
+  let vol = (444) * 10000
+  console.log("test add round")
+  await contracts.exchange.addround( vol, "1.1000 SEEDS", { authorization: `${exchange}@active` })  
+
+  let roundsAfterAdd = await getRounds()
+
+  assert({
+    given: `rounds price with 3.3% cumulative increase `,
+    should: `match values from spreadsheet`,
+    actual: [usd_rates[0], usd_rates[1], usd_rates[29], usd_rates[49]],
+    expected: [ "0.01100",  "0.01136", "0.02820", "0.05399"]
+  })
+
+  assert({
+    given: `add round `,
+    should: `be appended to rounds`,
+    actual: roundsAfterAdd.rows[50],
+    expected: {
+      "id": 50,
+      "max_sold": 50000000 + vol,
+      "seeds_per_usd": "1.1000 SEEDS"
+    }
+  })
+
+
+})
+
+describe('Basic Init Check', async assert => {
+
+  const contracts = await initContracts({ accounts, token, exchange })
+  
+  console.log(`reset exchange`)
+  await contracts.exchange.reset({ authorization: `${exchange}@active` })  
+
+  console.log(`init token sale rounds`)
+  await contracts.exchange.initsale({ authorization: `${exchange}@active` })
+  
+  let rounds = await eos.getTableRows({
+      code: exchange,
+      scope: exchange,
+      table: 'rounds',
+      json: true,
+      limit: 100,
+  })
+  
+  let usd_per_seeds = 0.011
+  let volume = 1100000
+  let results = []
+  for (let i=0; i<50; i++) {
+    let seeds_per_usd = 1 / usd_per_seeds
+
+    if (i==43) {
+      console.log("Round 44: usd_per_seeds: "+usd_per_seeds + " seeds_per_usd: "+seeds_per_usd)
+      // note: 225057.49999 is rounded by c++ to 225058 - doesn't matter given our precision requirements of 4 digits.
+    }
+    results.push({
+      volume: (volume * 10000)+"",
+      price: (Math.round(seeds_per_usd*10000.0)/10000.0).toFixed(4)
+    })
+
+    usd_per_seeds = usd_per_seeds * 1.033
+    volume += 1100000
+  }
+  for(let i=0; i<results.length; i++) {
+    assert({
+      given: 'round '+i,
+      should: 'have seeds per usd: ' + results[i].price + " SEEDS   Volume: "+results[i].volume,
+      actual: { 
+        price: rounds.rows[i].seeds_per_usd,
+        volume: rounds.rows[i].max_sold
+      },
+      expected: {
+        price: results[i].price + " SEEDS",
+        volume: results[i].volume
+      }
+    })
+    }
+
+
+
+})
+const getRounds = async () => {
+  let rounds = await eos.getTableRows({
+    code: exchange,
+    scope: exchange,
+    table: 'rounds',
+    json: true,
+    limit: 100,
+  })
+  return rounds
+}
+
+describe('Token Sale Price', async assert => {
+
+  const contracts = await initContracts({ accounts, token, exchange })
+  
+  console.log(`reset exchange`)
+  await contracts.exchange.reset({ authorization: `${exchange}@active` })  
+
+  console.log(`reset accounts`)
+  await contracts.accounts.reset({ authorization: `${accounts}@active` })
+
+  console.log(`add user`)
+  await contracts.accounts.adduser(firstuser, 'First user', "individual", { authorization: `${accounts}@active` })
+
+  console.log(`transfer seeds to ${exchange}`)
+  //await contracts.token.transfer(firstuser, exchange, "2000000.0000 SEEDS", 'unit test', { authorization: `${firstuser}@active` })
+
+  console.log(`update daily limits`)
+  await contracts.exchange.updatelimit("100.0000 SEEDS", "10.0000 SEEDS", "3000000.0000 SEEDS", { authorization: `${exchange}@active` })
+  
+  console.log(`update TLOS rate - tlos per usd`)
+  await contracts.exchange.updatetlos("3.0000 SEEDS", { authorization: `${exchange}@active` })
+  //await contracts.exchange.updateusd("1.0000 SEEDS", { authorization: `${exchange}@active` })
+
+  console.log(`init token sale rounds`)
+
+  console.log("test init rounds - 10.0000 seeds per round")
+
+  for (let i=0; i<12; i++) {
+    await contracts.exchange.addround( 10 * 10000, i+1+".0000 SEEDS", { authorization: `${exchange}@active` })  
+  }
+
+  //console.log("rounds "+JSON.stringify(await getRounds(), null, 2))
+
+  let b1 = await getBalanceFloat(firstuser)
+
+  const updateprice = async (expected_round, expected_price, expected_remaining) => {
+
+    let price = await eos.getTableRows({
+      code: exchange,
+      scope: exchange,
+      table: 'price',
+      json: true,
+    })
+    //console.log("price: "+JSON.stringify(price, null, 2))
+
+    assert({
+      given: `new price`,
+      should: `expected round: `+expected_round + " price: "+expected_price + " remaining "+expected_remaining,
+      actual: price.rows[0],
+      expected: {
+        "id": 0,
+        "current_round_id": expected_round,
+        "current_seeds_per_usd": parseInt(expected_price)+".0000 SEEDS",
+        "remaining": expected_remaining
+      }
+    })
+
+  }
+
+  const check = async (initial, expectedDelta, usd) => {
+    let balance = await getBalanceFloat(firstuser)
+    let delta = balance - initial
+    //console.log(" expected "+expectedDelta + " actual " + delta + " initial "+ initial + " balance: "+ balance)
+
+    assert({
+      given: `purchase `+usd+" usd worth",
+      should: `receive expected number of Seeds: `+expectedDelta,
+      actual: delta.toFixed(4),
+      expected: expectedDelta.toFixed(4)
+    })
+  
+  }
+
+  console.log("buy seeds 10 USD")
+
+  // edge case, buy entire round, check price after
+  await contracts.exchange.newpayment(firstuser, "BTC", "0", parseInt(10 * 10000), { authorization: `${exchange}@active` })
+
+  let seedsBalance = 10
+
+  await check(b1, seedsBalance, 10)
+
+  b1 += seedsBalance
+
+  await updateprice(1, 2, 10 * 10000)
+
+
+  console.log("buy seeds 0.5 USD")
+
+  await contracts.exchange.newpayment(firstuser, "BTC", "01", parseInt(5000), { authorization: `${exchange}@active` })
+
+  await check(b1, 1, 0.5)
+
+  b1 += 1
+
+  await updateprice(1, 2, 9 * 10000)
+
+  console.log("buy seeds 6 USD") 
+
+  // 9 seeds left at 2 seeds / usd = 4.5 USD
+  // 1.5 * 3 seeds = 4.5
+
+  //9 + 4.5 = 13.5
+
+  //11 + 13.5 = 24.5
+
+  // cross over into a new round - 
+  await contracts.exchange.newpayment(firstuser, "BTC", "02", parseInt(6 * 10000), { authorization: `${exchange}@active` })
+
+  await check(b1, 13.5, "")
+
+  b1 += 13.5
+
+
+  await updateprice(2, 3,  55000)
+
+  // 8.22 usd
+
+  // 5.5 S remain at 3 s/usd
+  // 5.5 .... 1.833333333 USD ==> 5.5 Seeds
+
+  // remain: 6.3866666667
+
+  // 10 S at 4/usd 2.5 usd 10 Seeds
+  // 10 at 5 => 2 usd      10 Seeds
+  // 10 at 6 => 1.666666   10 Seeds
+  // 10 at 7 => 0.22 USD buys 1.54 Seeds
+
+  // total: 37.04
+
+  console.log("buy seeds 8.22 USD")
+
+  await contracts.exchange.newpayment(firstuser, "BTC", "04", parseInt(8.22 * 10000), { authorization: `${exchange}@active` })
+
+  //61.54
+  await check(b1, 37.04, "")
+
+  await updateprice(6, 7, 84600)
+
+
+
+})
+
+
+describe('Token Sale 50 Rounds', async assert => {
+
+  const contracts = await initContracts({ accounts, token, exchange })
+  
+  console.log(`reset exchange`)
+  await contracts.exchange.reset({ authorization: `${exchange}@active` })  
+
+  console.log(`reset accounts`)
+  await contracts.accounts.reset({ authorization: `${accounts}@active` })
+
+  console.log(`add user`)
+  await contracts.accounts.adduser(firstuser, 'First user', "individual", { authorization: `${accounts}@active` })
+
+  console.log(`transfer seeds to ${exchange}`)
+  //await contracts.token.transfer(firstuser, exchange, "2000000.0000 SEEDS", 'unit test', { authorization: `${firstuser}@active` })
+
+  console.log(`update daily limits`)
+  await contracts.exchange.updatelimit("100.0000 SEEDS", "10.0000 SEEDS", "3000000.0000 SEEDS", { authorization: `${exchange}@active` })
+  
+  console.log(`update TLOS rate - tlos per usd`)
+  await contracts.exchange.updatetlos("3.0000 SEEDS", { authorization: `${exchange}@active` })
+  //await contracts.exchange.updateusd("1.0000 SEEDS", { authorization: `${exchange}@active` })
+
+  console.log(`init token sale rounds`)
+
+  console.log("test init rounds - 10.0000 seeds per round")
+  
+  let usd_per_seeds = 0.011
+
+  var total_usd = 0
+
+  for (let i=0; i<50; i++) {
+
+    total_usd = total_usd + usd_per_seeds * 10
+
+    let seeds_per_usd = 1 / usd_per_seeds
+
+    //console.log("round "+i+" = "+seeds_per_usd.toFixed(4)+" SEEDS")
+
+    await contracts.exchange.addround( 10 * 10000, seeds_per_usd.toFixed(4)+" SEEDS", { authorization: `${exchange}@active` })  
+
+    usd_per_seeds = usd_per_seeds * 1.033
+
+  }
+
+  //console.log("rounds "+JSON.stringify(await getRounds(), null, 2))
+
+  console.log("total value "+total_usd)
+
+  let b1 = await getBalanceFloat(firstuser)
+
+  const updateprice = async (expected_round, expected_price, expected_remaining) => {
+
+    let price = await eos.getTableRows({
+      code: exchange,
+      scope: exchange,
+      table: 'price',
+      json: true
+    })
+    //console.log("price: "+JSON.stringify(price, null, 2))
+
+    assert({
+      given: `new price`,
+      should: `expected round: `+expected_round + " price: "+expected_price + " remaining "+expected_remaining,
+      actual: price.rows[0],
+      expected: {
+        "id": 0,
+        "current_round_id": expected_round,
+        "current_seeds_per_usd": expected_price.toFixed(4)+" SEEDS",
+        "remaining": expected_remaining
+      }
+    })
+
+    let config = await eos.getTableRows({
+      code: exchange,
+      scope: exchange,
+      table: 'config',
+      json: true,
+    })
+
+    let seedsPerUsdString = expected_price.toFixed(4) + " SEEDS"
+    
+    assert({
+      given: 'config table price',
+      should: "price: "+expected_price,
+      actual: config.rows[0].seeds_per_usd,
+      expected: seedsPerUsdString
+    })
+
+  }
+
+  const check = async (initial, expectedDelta, usd) => {
+    let balance = await getBalanceFloat(firstuser)
+    let delta = balance - initial
+    //console.log(" expected "+expectedDelta + " actual " + delta + " initial "+ initial + " balance: "+ balance)
+
+    assert({
+      given: `purchase `+usd+" usd worth",
+      should: `receive expected number of Seeds: `+expectedDelta,
+      actual: delta.toFixed(4),
+      expected: expectedDelta.toFixed(4)
+    })
+  
+  }
+
+  console.log("buy 1 seeds 0.011 USD")
+  let totalbuy = 0
+  let buy = 0.011
+  totalbuy += buy
+
+  await contracts.exchange.newpayment(firstuser, "BTC", "0", parseInt(buy * 10000), { authorization: `${exchange}@active` })
+
+  let seedsBalance = 1
+
+  await check(b1, seedsBalance, 0.011)
+
+  b1 += seedsBalance
+
+  await updateprice(0, 90.9091, 9 * 10000)
+
+  buy = 0.099
+  totalbuy += buy
+
+  console.log("buy seeds USD"+buy)
+  await contracts.exchange.newpayment(firstuser, "BTC", "01", parseInt(buy * 10000), { authorization: `${exchange}@active` })
+  seedsBalance = 9
+  await check(b1, seedsBalance, buy)
+  b1 += seedsBalance
+  await updateprice(1, 88.0049, 10 * 10000)
+
+
+  // 13.3 seeds = 10 seeds at round 2 price of 88.0049 ==> 0.1136300365
+  // 3.3 seeds are round 3 price of 85.1935 ==> 0.03873534953
+
+  // == 0.152365386
+
+  buy = 0.1524 // 65386
+  totalbuy += buy
+
+  console.log("buy seeds "+buy) 
+
+  await contracts.exchange.newpayment(firstuser, "BTC", "02", parseInt(buy * 10000), { authorization: `${exchange}@active` })
+
+  await check(b1, 13.3029, buy)
+
+  b1 += 13.3029
+
+  await updateprice(2, 85.1935,  (10 - 3.3029) * 10000)
+
+  console.log("bought "+totalbuy + " out of "+total_usd) 
+
+
+  console.log("buy until last round ") 
+  await contracts.exchange.newpayment(firstuser, "BTC", "03999", parseInt( (total_usd - totalbuy - 0.1) * 10000), { authorization: `${exchange}@active` })
+  await updateprice(49, 18.5222,  18538)
+
+  console.log("buy in last round ") 
+  await contracts.exchange.newpayment(firstuser, "BTC", "04ss", parseInt(0.001 * 10000), { authorization: `${exchange}@active` })
+  await updateprice(49, 18.5222, Math.ceil(18538 - (18.5222*0.001*10000)))
+
+  console.log("buy more than last round ") 
+  let out_of_funds_purchase = false
+  try {
+    await contracts.exchange.newpayment(firstuser, "BTC", "05aaaa", parseInt(1 * 10000), { authorization: `${exchange}@active` })
+    out_of_funds_purchase = true
+  } catch (err) {
+    //console.log("expected error "+err) 
+  }
+  assert({
+    given: 'purchase more than available in rounds',
+    should: 'fail',
+    actual: out_of_funds_purchase,
+    expected: false
+  })
+
+
 })
