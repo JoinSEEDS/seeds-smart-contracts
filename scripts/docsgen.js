ac65c5bff45cba814600bd3a1fa4c140a27a148f
Julio Holon <juliolrmonteiro@gmail.com>
2020-11-22 22:49:42 -0300

    Added complete template for docs, and regenerate previous comments

--- scripts/docsgen.js
+++ scripts/docsgen.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const fs = require(`fs`)
 const path = require(`path`)
-const Mustache = require('mustache')
+const Handlebars = require("handlebars");
 const cheerio = require('cheerio')
 const commonmark = require('commonmark')
 const { Serialize, api } = require(`eosjs`)
@@ -11,11 +11,7 @@ const { promisify } = require('util')
 
 const { source } = require('./deploy')
 
-const existsAsync = promisify(fs.exists)
-const mkdirAsync = promisify(fs.mkdir)
-const unlinkAsync = promisify(fs.unlink)
 const readFileAsync = promisify(fs.readFile)
-const execAsync = promisify(exec)
 
 const docsgen = async (contract) => {
     try {
@@ -23,11 +19,36 @@ const docsgen = async (contract) => {
         const outputPath = path.join(__dirname, '../docs', contract.concat('.html'))
         const templatePath = path.join(__dirname, '../docs/docstemplate.html')
         var abiFile = await readFileAsync(abiPath, "utf8")
+        var abiContents = JSON.parse(abiFile);
+        abiContents["file"] = contract;
+
+        // Imports comments from previous genrated page
+        if (fs.existsSync(outputPath)) {
+            var outFile = await readFileAsync(outputPath, "utf8")
+            const $ = cheerio.load(outFile)
+            $('#target comment').each(function(i, elem) {
+                if ($(this).attr("type") == "main") {
+                    abiContents["comment"] = $(this).text();
+                } else {
+                    const desired_type = $(this).attr("type")+"s"; 
+                    const desired_name = $(this).attr("name");
+                    var old = abiContents[desired_type].find(function (e) { 
+                        return e["name"]==desired_name;
+                    });
+                    if (old) {
+                        var pos = abiContents[desired_type].indexOf(old)
+                        abiContents[desired_type][pos]["comment"] = $(this).text();    
+                    }
+        
+                }
+            });
+        }
         var templateFile = await readFileAsync(templatePath, "utf8")
         console.log(`generating docs for: ${contract}`)
         const $ = cheerio.load(templateFile)
-        var template = $('#template').html();
-        var rendered = Mustache.render(template, JSON.parse(abiFile));
+        var templateCode = $('#template').html();
+        const template = Handlebars.compile(templateCode);
+        var rendered = template(abiContents);
         $('#target').html(rendered);
         var reader = new commonmark.Parser();
         var writer = new commonmark.HtmlRenderer();
