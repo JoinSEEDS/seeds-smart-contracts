714cd6205202ef8c4a2d7962032a1a4c0d2c6b89
nik <nheger@gmail.com>
2020-12-09 14:07:43 +0800

    storing negative mint rate
    I asked for this to be 0 but Ian convinced me it's better to store a negative mint rate and deal with the negative number in the harvest calculaiton. We don't want to lose ths info.

--- src/seeds.harvest.cpp
+++ src/seeds.harvest.cpp
@@ -940,8 +940,6 @@ void harvest::calcmintrate () {
 
   double mint_rate = delta / 708.0;
 
-  if (mint_rate < 0) { mint_rate = 0; }
-
   auto mitr = mintrate.begin();
   if (mitr != mintrate.end()) {
     mintrate.modify(mitr, _self, [&](auto & item){
