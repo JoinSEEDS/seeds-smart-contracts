341b2042120fc5ed7dbce359e6f1f6a0d744f052
nik <nheger@gmail.com>
2021-04-03 18:48:51 +0800

    script

--- accountinfo.sh
+++ accountinfo.sh
@@ -7,5 +7,13 @@ cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  funds.seeds 
 echo "REP"
 cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  accts.seeds accts.seeds rep 
 
+echo "REFERER"
+cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  accts.seeds accts.seeds refs
+
+echo "VOUCH"
+cleos -u https://node.hypha.earth/ get table accts.seeds $1 vouches
+
+echo "VOUCH TOTALS"
+cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  accts.seeds accts.seeds vouchtotals
 
 #cleos -u https://node.hypha.earth/ get table funds.seeds loveandlight voice --limit 300
