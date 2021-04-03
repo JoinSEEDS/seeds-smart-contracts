c7eaadd023968031f573a5a706ebd35504c7312f
nik <nheger@gmail.com>
2021-04-03 18:01:09 +0800

    utility update

--- accountinfo.sh
+++ accountinfo.sh
@@ -1,6 +1,11 @@
+echo "ACCOUNT"
 cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  accts.seeds accts.seeds users
 
+echo "VOICE"
 cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  funds.seeds funds.seeds voice 
+
+echo "REP"
+cleos -u https://node.hypha.earth/ get table --lower $1 --upper $1  accts.seeds accts.seeds rep 
 
 
 #cleos -u https://node.hypha.earth/ get table funds.seeds loveandlight voice --limit 300
