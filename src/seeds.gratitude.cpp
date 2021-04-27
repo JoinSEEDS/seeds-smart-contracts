517cbebf4c6f06cadc6293494699f16dc8a0ac09
Julio L R Monteiro <juliolrmonteiro@gmail.com>
2021-04-26 23:53:47 -0300

    Feature/gratitude acknowledge (#299)
    * Initial acknowledge impl
    
    * Added different values for citizens and residents
    
    * Single point to initialize gratz balance on account
    
    * Calculating ack split
    
    * fixed some tests
    
    * tests still don't work
    
    * Fixed testacks and all tests passing
    
    * Checking if the user is a Seeds user before giving gratz

--- src/seeds.gratitude.cpp
+++ src/seeds.gratitude.cpp
@@ -9,6 +9,11 @@ ACTION gratitude::reset () {
     bitr = balances.erase(bitr);
   }
 
+  auto actr = acks.begin();
+  while (actr != acks.end()) {
+    actr = acks.erase(actr);
+  }
+
   auto sitr = sizes.begin();
   while (sitr != sizes.end()) {
     sitr = sizes.erase(sitr);
@@ -31,10 +36,11 @@ ACTION gratitude::reset () {
 ACTION gratitude::give (name from, name to, asset quantity, string memo) {
   require_auth(from);
 
-  check( from != to, "gratitude: cannot give to self" );
-  check( is_account( to ), "gratitude: to account does not exist");
+  check(from != to, "gratitude: cannot give to self");
+  check(is_account(to), "gratitude: to account does not exist");
+  check_user(to);
   check_asset(quantity);
-  check( quantity.amount > 0, "gratitude: must give positive quantity" );
+  check(quantity.amount > 0, "gratitude: must give positive quantity");
 
   // Should create balances if not there yet
   init_balances(to);
@@ -46,21 +52,54 @@ ACTION gratitude::give (name from, name to, asset quantity, string memo) {
   update_stats(from, to, quantity);
 }
 
+ACTION gratitude::acknowledge (name from, name to, string memo) {
+  require_auth(from);
+
+  check(from != to, "gratitude: cannot give to self");
+  check(is_account(to), "gratitude: to account does not exist");
+  check_user(to);
+
+  auto actr = acks.find(from.value);
+  if (actr == acks.end()) {
+    acks.emplace(_self, [&](auto& item) {
+      item.donor = from;
+      item.receivers = vector{to};
+    });
+  } else {
+    acks.modify(actr, get_self(), [&](auto &item) {
+        item.receivers.push_back(to);
+    });
+  }
+}
+
+ACTION gratitude::testacks() {
+  require_auth(get_self());
+
+  auto actr = acks.begin();
+  while (actr != acks.end()) {
+    calc_acks(actr->donor);
+    actr = acks.erase(actr);
+  }
+}
+
 ACTION gratitude::newround() {
   require_auth(get_self());
-  auto generated_gratz = config_get(gratzgen);
 
   auto contract_balance = eosio::token::get_balance(contracts::token, get_self(), seeds_symbol.code());
   uint64_t tot_accounts = get_size("balances.sz"_n);
   uint64_t volume = get_current_volume();
 
+  auto actr = acks.begin();
+  while (actr != acks.end()) {
+    calc_acks(actr->donor);
+    actr = acks.erase(actr);
+  }
+
   auto bitr = balances.begin();
   while (bitr != balances.end()) {
     uint64_t my_received = bitr->received.amount;
-    balances.modify(bitr, _self, [&](auto& item) {
-        item.received = asset(0, gratitude_symbol);
-        item.remaining = asset(generated_gratz, gratitude_symbol);
-    });
+    // reset gratitude for account
+    init_balances(bitr->account);
     float split_factor = my_received / (float)volume;
     uint64_t payout = contract_balance.amount * split_factor;
     // Pay out SEEDS in store
@@ -74,6 +113,35 @@ ACTION gratitude::newround() {
 void gratitude::check_user (name account) {
   auto uitr = users.find(account.value);
   check(uitr != users.end(), "gratitude: user not found");
+}
+
+void gratitude::calc_acks (name donor) {
+  auto bitr = balances.find(donor.value);
+  uint64_t remaining = bitr->remaining.amount;
+
+  auto min_acks = config_get(gratz_acks);
+
+  auto actr = acks.find(donor.value);
+  if (actr != acks.end()) {
+    std::map<name, uint64_t> unique_recs;
+    for (std::size_t i = 0; i < actr->receivers.size(); i++) {
+      unique_recs[actr->receivers[i]]++;
+    }
+    auto uritr = unique_recs.begin();
+    while (uritr != unique_recs.end()) {
+      uint64_t received = 0;
+      if (actr->receivers.size() < min_acks) {
+        received = (remaining / min_acks) * uritr->second;
+      } else {
+        received = (remaining / actr->receivers.size()) * uritr->second;
+      }
+
+      sub_gratitude(donor, asset(received, gratitude_symbol));
+      add_gratitude(uritr->first, asset(received, gratitude_symbol));
+      update_stats(donor, uritr->first, asset(received, gratitude_symbol));
+      uritr++;
+    }
+  }
 }
 
 uint64_t gratitude::get_current_volume() {
@@ -98,7 +166,20 @@ void gratitude::update_stats(name from, name to, asset quantity) {
 void gratitude::init_balances (name account) {
   auto bitr = balances.find(account.value);
 
-  auto generated_gratz = config_get(gratzgen);
+  uint64_t generated_gratz = 0;
+  auto uitr = users.find(account.value);
+  if (uitr != users.end()) {
+    switch (uitr->status)
+    {
+    case "citizen"_n:
+      generated_gratz = config_get(gratzgen_cit);
+      break;
+    
+    case "resident"_n:
+      generated_gratz = config_get(gratzgen_res);
+      break;
+    }
+  }
 
   if (bitr == balances.end()) {
     balances.emplace(_self, [&](auto & item){
@@ -107,6 +188,11 @@ void gratitude::init_balances (name account) {
       item.remaining = asset(generated_gratz, gratitude_symbol);
     });
     size_change("balances.sz"_n, 1);
+  } else {
+    balances.modify(bitr, _self, [&](auto& item) {
+        item.received = asset(0, gratitude_symbol);
+        item.remaining = asset(generated_gratz, gratitude_symbol);
+    });
   }
 }
 
