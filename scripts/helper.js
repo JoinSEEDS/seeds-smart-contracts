159b25d2e7ca715fa3d6934581ef244ba917dabc
NIK <nheger@gmail.com>
2021-08-24 14:00:42 +0800

    our test node is too slow, using telos foundation

--- scripts/helper.js
+++ scripts/helper.js
@@ -28,7 +28,7 @@ const networkDisplayName = {
 const endpoints = {
   local: 'http://127.0.0.1:8888',
   kylin: 'http://kylin.fn.eosbixin.com',
-  telosTestnet: 'https://test.hypha.earth',
+  telosTestnet: 'https://api-test.telosfoundation.io',
   telosMainnet: 'https://api.telosfoundation.io'
 }
 
